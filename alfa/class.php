498c8f5ff1d97d0ebdf468cd5d962100d24a6efc
Alex <alexsandakovmsk@yandex.ru>
2021-01-31 00:23:00 +0300

    отредактировал1

--- alfa/class.php
+++ alfa/class.php
@@ -4,7 +4,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 use \Bitrix\Main;
 use Bitrix\Main\Web\Json;
 use \Bitrix\Main\Localization\Loc as Loc;
-// ввв
+//
 
 class AlfaSite extends CBitrixComponent
 {
